0f641bfbfa0563465847a9ef1e5085fe38a2863a
Satrio <sh999gh@gmail.com>
2016-09-21 06:26:40 -0400

    Trying to count filetypes. Will change url frontier to set

--- crawl.py
+++ crawl.py
@@ -31,46 +31,46 @@ def ignore_from_setdelay(crawler_delay):
 	else:
 		return False
 
-def get_links(robots, url, url_frontier, outfile, domains_out, domain, limit_domain):	
+def get_links(robots, url, url_frontier, subdomains, outfile, domains_out, domain, limit_domain):	
 	try:
 		print "URL:", url
 		print "Checking robots..."
-		crawler_delay = robots.delay(url, 'my-agent')
-		# delay_crawler(crawler_delay)
-		# if ignore_from_setdelay(crawler_delay):
-		# 	print "Skipping due to crawl_delay"
-		# 	return url_frontier
-		if crawler_delay != None and crawler_delay > 2.0:
+		crawler_delay = robots.delay(url, 'my-agent')		# Get craw-delay if it exists
+		if crawler_delay != None and crawler_delay > 5.0:
 			# If there is delay limit, either skip site or wait for the time specified (choose randomly)
 			r = random.uniform(0,10)
 			if r > 2:	# Skip 80% of the time here
 				print "Skipping due to delay"
 				return url_frontier
 			else:
-				print "Complying with delay"
+				print "Slowing crawl"
 				time.sleep(crawler_delay)
 		if (robots.allowed(url, "*")):	# Allowed by robots
 			print "Allowed to visit"
 			if len(url_frontier) < 50:  # Ensure first site gets read
 				t = 20
 			else:   # Other sites limit
-				t = 2
-			# site = urllib2.urlopen(url, timeout=t)
+				t = 5
 			print "Requesting header"
 			request = urllib2.Request(url)
-			site = urllib2.urlopen(request, timeout=t)	
-			content_type = site.info().getheader('Content-Type')
+			site = urllib2.urlopen(request, timeout=t)		# Open site
+			content_type = site.info().getheader('Content-Type')	# Get header to get filetype
 			content_type_regex = r"(text/html)"
-			print "Content_type:", content_type
+			print "Content_type from header:", content_type
 			match = re.search(content_type_regex, content_type)
 			if match != None:		# filetype is what we want, text/html
 				print "\tmatch:", match.group(1)
 				if match.group(1) == 'text/html':
-					print "\tThis is text/html type"
+					print "\Detected text/html type"
 					# site_soup = BeautifulSoup(site)			# Convert to tree of tags soup object
 					# tag_a_elements = site_soup.find_all("a")		# Get all tags <a> from tag tree
+				elif match.group(1) != 'text/html':
+					print "\tDetected type:", match.group(1)
+					subdomains.add(match.group(1))
+					domains_out.write(match.group(1))
+					return url_frontier
 				else:	# filetype not html, so skip
-					print "\tUnwanted type; not opening"
+					print "\No type detected; not opening"
 					return url_frontier
 			else:	# regex didn't get filetype (should not happen)
 				print "No match"
@@ -128,13 +128,15 @@ def main():
 	visit_num = 1
 	my_domain = "uky.edu"
 	start_url = "http://www."+my_domain
+	start_url = "http://www.msc.uky.edu"
 	url_frontier = [start_url]
+	subdomains = set()
 	# url_frontier = get_links(start_url, url_frontier, outfile, domains_out, domain="umich.edu", limit_domain=True)
 	robots = RobotsCache()
 
 	for url in url_frontier:
 		print "\nVisit #:",visit_num
-		url_frontier = get_links(robots, url, url_frontier, outfile, domains_out, domain=my_domain, limit_domain=True)
+		url_frontier = get_links(robots, url, url_frontier, subdomains, outfile, domains_out, domain=my_domain, limit_domain=True)
 		visit_num += 1
 	outfile.close()
 	domains_out.close()
